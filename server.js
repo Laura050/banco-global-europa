8a1ff817c3a1e73e02a8da92d3477b8f409eecef
Laura050 <nizbol72@gmail.com>
2025-01-25 20:35:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const authRoutes = require('./routes/auth');
+const adminRoutes = require('./routes/admin');
+
 const app = express();
 
 app.use(cors());
@@ -13,6 +16,9 @@ app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/admin', adminRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
