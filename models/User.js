cf742cb277a5359b886018b75fbb0b418b84005e
Laura050 <nizbol72@gmail.com>
2026-06-29 00:25:46 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,20 +1,19 @@
 const mongoose = require('mongoose');
-
 const userSchema = new mongoose.Schema({
-  nombre: String,
-  apellidos: String,
+  nombre: String, 
+  apellidos: String, 
   telefono: String,
-  email: { type: String, unique: true },
+  email: { type: String, unique: true }, 
   dni: { type: String, unique: true },
-  pais: String,
-  ciudad: String,
-  codigoPostal: String,
+  pais: String, 
+  ciudad: String, 
+  codigoPostal: String, 
   domicilio: String,
-  password: String,
+  password: String, 
   numeroCuenta: String,
+  devise: { type: String, default: '€' },
   saldo: { type: Number, default: 0 },
   estado: { type: String, default: 'pendiente' },
-  isAdmin: { type: Boolean, default: false }  // Ajout du champ isAdmin
+  isAdmin: { type: Boolean, default: false }
 });
-
 module.exports = mongoose.model('User', userSchema);
