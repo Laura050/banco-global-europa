b14c60071c2ced3743007a70039cb35a37349c35
Laura050 <nizbol72@gmail.com>
2026-06-29 02:53:32 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -18,13 +18,10 @@ router.get('/profile', async function(req, res) {
 // Obtenir les informations du compte
 router.get('/account', async function(req, res) {
   try {
-    const user = await User.findById(req.userData.userId).select('numeroCuenta saldo estado');
-    if (!user) {
-      return res.status(404).json({ error: 'Usuario no encontrado' });
-    }
+    const user = await User.findById(req.userData.userId).select('numeroCuenta saldo estado devise');
     res.json(user);
   } catch (error) {
-    res.status(500).json({ error: 'Error al obtener cuenta' });
+    res.status(500).json({ error: error.message });
   }
 });
 
